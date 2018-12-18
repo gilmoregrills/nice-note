16bb32b2963148483898ae5dee9305bda6fef5cf
gilmoregrills <robinfarrowyonge@gmail.com>
2018-12-18 14:26:55 +0000

    change routes to single webhook

--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,9 @@
 module.exports = function(app) {
-    app.post('/create', (req, res) => {
-        // You'll create your note here.
-        console.log(req.body)
-        var parameters = req.body["queryResult"]["parameters"]
-        res.send('Creating a new note')
-    });
-
-    app.post('/update', (req, res) => {
-        // You'll create your note here.
-        res.send('Updating your note')
-    });
-
-    app.post('/delete', (req, res) => {
-        // You'll create your note here.
-        res.send('Deleting your note')
-    });
-
-    app.post('/read', (req, res) => {
-        // You'll create your note here.
-        res.send('Fetching your note')
-    });
-
     app.post('/', (req, res) => {
         // You'll create your note here.
-        res.send('This is home')
+        console.log(req.body)
+        res.send({
+            "fulfillmentText": "This is a text response"
+        })
     });
 };
