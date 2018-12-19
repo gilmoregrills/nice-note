ab1ad90fa2273f5afc7963499f4b5167eaf6aab6
gilmoregrills <robinfarrowyonge@gmail.com>
2018-12-19 09:07:12 +0000

    fix healthchecking maybe?

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = 8443
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendStatus(200))
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
