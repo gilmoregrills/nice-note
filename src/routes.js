42912c898f5ec9673567aa6ff19f4b168aa0b7cb
gilmoregrills <robinfarrowyonge@gmail.com>
2018-12-21 17:54:06 +0000

    parse request ready to extract search terms

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
 module.exports = function(app) {
     app.post('/webhook', (req, res, next) => {
         // You'll create your note here.
-        console.log(Object.keys(req))
-        console.log(req.body)
+        console.log(req.body['queryResult'])
+
+        var intent = req.body['queryResult']['intent']['displayName']
+        var keywords = req.body['queryResult']['parameters']['keyword']
+        var user = req.body['queryResult']['parameters']['user']
+        var plural = req.body['queryResult']['parameters']['plural']
+        var date = req.body['queryResult']['parameters']['date']
+        var timePeriod = req.body['queryResult']['parameters']['time-period']
+        var datePeriod = req.body['queryResult']['parameters']['date-period']
+
         res.send({
-            "fulfillmentText": "textetxtetxt"
+            "fulfillmentText": "executing function for intent: " + intent
         })
     });
 };
