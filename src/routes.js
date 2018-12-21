f57cdee41998471a541d7b39487fed599c616e56
gilmoregrills <robinfarrowyonge@gmail.com>
2018-12-21 17:03:36 +0000

    update routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
+var bodyParser = require('body-parser')
+
 module.exports = function(app) {
     app.post('/webhook', (req, res) => {
         // You'll create your note here.
+        console.log(Object.keys(req['next']))
         console.log(req.body)
         res.send({
-            "fulfillmentText": "This is a text response"
+            "fulfillmentText": "session : " + req['body'].toString()
         })
     });
 };
